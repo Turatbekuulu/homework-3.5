25bce79cf3f9ddd86e1a0a6f770527328e89a260
Arsen <turatbekuuluarsen@gmail.com>
2022-06-18 03:36:34 +0600

    Initial Commit

---
+++ homework 3.5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework 3.5
+//
+//  Created by Mac on 18/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
